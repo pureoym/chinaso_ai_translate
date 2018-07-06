e02584c955a727330d936bc8f3abbf8e83400548
pureoym <pureoym@163.com>
2018-07-06 16:57:20 +0800

    test

--- utils.py
+++ utils.py
@@ -28,8 +28,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-APP_ID = '20180706000183232'
-SECRET_KEY = '4Ay350mc66H_h_TyNYpZ'
+
 API_URL = '/api/trans/vip/translate'
 Q = 'apple'
 FROM_LANGUAGE = 'zh'
